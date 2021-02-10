95820d818c19c4ec4ffb25e8b5c83d65aa27eb3c
dibazi <bertod48@yahoo.fr>
2021-02-10 18:11:28 +0200

    wp-senga

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', ltrim($DATABASE_URL["path"], "/") );
+define( 'DB_NAME', 'wp-senga' );
 
 /** MySQL database username */
-define( 'DB_USER', $DATABASE_URL["user"] );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $DATABASE_URL["pass"] );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
-define( 'DB_HOST', $DATABASE_URL["host"] );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
